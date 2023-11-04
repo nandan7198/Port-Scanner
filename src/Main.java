6aad5d1b120829f99f3b8c3fd377bb9d798f39ef
nandan7198 <99603611+nandan7198@users.noreply.github.com>
2023-11-04 14:58:46 +0530

    Commit

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Main {
+
+    private JMenuBar menubar;
+    private JMenu menu;
+    private JMenuItem option, option2;
+    private JFrame frame;
+    private JTextArea textbox;
+    private JScrollPane scroll;
+
+    public Main() {
+
+        Panel panel = new Panel();
+         
+        menubar = new JMenuBar();
+        menu = new JMenu("Options");
+        option = new JMenuItem("Port Range");
+        option2 = new JMenuItem("Host Resolve");
+
+        menubar.add(menu);
+        menu.add(option);
+        menu.add(option2);
+
+        frame = new JFrame("Port Scanner v1.0");
+
+        frame.setJMenuBar(menubar);
+
+        frame.setResizable(false);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(panel);
+        frame.setSize(400, 600);
+
+        option.addActionListener(new MenuListener());
+        option2.addActionListener(new MenuListener2());
+
+        frame.setVisible(true);
+
+    }
+
+    /**
+     * Basically runs the program
+     */
+    public static void main(String[] args) {
+
+        Main main = new Main();
+
+    }
+
+    private class MenuListener2 implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+
+            String hostname = JOptionPane.showInputDialog(null, "Enter a hostname");
+            HostResolve hr = new HostResolve(hostname.trim());
+
+            hr.addressResolve(hostname);
+
+            }
+
+        }
+
+        /**
+         * Menu listener that allows the user to specify the range of ports to scan
+         */
+        private class MenuListener implements ActionListener {
+
+            public void actionPerformed(ActionEvent event) {
+
+                Scan scan = new Scan();
+
+                String port1 = JOptionPane.showInputDialog(null, "Enter the first port range");
+                String port2 = JOptionPane.showInputDialog(null, "Enter the second port range");
+
+                scan.setRange(port1, port2);
+
+            }
+
+        }
+
+    }
---
+++ src/Scan.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+public class Scan {
+
+    private static Integer first, second;
+    private List<Future<ScanResult>> futures = new ArrayList<>();
+
+    public Scan() {
+
+    }
+
+    /**
+     * Sets the range of the port scanner
+     */
+    public void setRange(String port1, String port2) {
+
+        try {
+
+            Integer num1 = Integer.parseInt(port1);
+            Integer num2 = Integer.parseInt(port2);
+
+            second = num2;
+            first = num1;
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("Please specify a port range!");
+
+        }
+
+        }
+
+        public boolean isRangeSet() {
+
+            boolean which;
+
+            if (first == null) {
+                which = false;
+            } else
+                which = true;
+
+            return which;
+        }
+
+    public void startScan(String host) throws Exception  {
+
+        final ExecutorService es = Executors.newFixedThreadPool(100);
+
+        if (isRangeSet() == false) {
+
+            System.out.println("You did not set a port range!");
+
+        } else
+
+        for (int i = first; i <= second; i++) {
+            futures.add(portScan(es, host, i));
+        }
+
+        es.shutdown();
+
+        for (final Future<ScanResult> f : futures) {
+            if (f.get().getOpen()) {
+
+                System.out.println("Port " + f.get().getPort() + " is open!");
+            }
+        }
+
+    }
+
+
+    public static Future<ScanResult> portScan(final ExecutorService es, String host, int port) {
+
+        return es.submit(new Callable<ScanResult>() {
+
+                             @Override
+                             public ScanResult call() {
+
+                                 ScanResult result = new ScanResult(port, false);
+
+                                 try {
+
+                                     Socket socket = new Socket();
+
+                                     socket.connect(new InetSocketAddress(host, port), 200);
+                                     socket.close();
+
+                                     result = new ScanResult(port, true);
+
+                                     return result;
+
+                                 } catch (Exception e) {
+
+
+                                 }
+                                 return result;
+                             }
+
+                         }
+        );
+}
+}
